1021ba83f11666cd8b909236a5759ded22bd1306
BiancaGiovanna <biancagothicg@gmail.com>
2021-03-05 15:12:20 -0300

    consertando a produção

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
 
 module.exports = {
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_DB,
-  dialect: "mysql",
-  define: {
-    timestamp: true,
-    underscored: true,
+  url: process.env.DATABASE_URL,
+  config: {
+    dialect: "mysql",
+    define: {
+      timestamp: true,
+      underscored: true,
+    },
   },
 };
